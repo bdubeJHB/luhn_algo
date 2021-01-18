3e81cad96212afed7a3ae6c6d73e88978560fd95
bdubeJHB <bdube@student.wethinkcode.co.za>
2021-01-18 22:02:23 +0200

    Modifying the tests and making the environment recognize the tests better

--- src/test/java/com/runaddictmusic/luhn/PersonTest.java
+++ src/test/java/com/runaddictmusic/luhn/PersonTest.java
@@ -1,24 +1,59 @@
 package com.runaddictmusic.luhn;
 
-import org.junit.Jupiter.api.Test;
+
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 
 public class PersonTest
 {
     @Test
-    public void doesNotAcceptInvalidStrings()
+    public void personInstanceCounterWorks()
     {
-        Person ben = new Person("42082fybtp0rz");
-        assertFalse(ben.validate_number(ben.get_id()));
+        assertEquals(0, Person.get_count());
+
+        Person test1 = new Person("44444444");
+        assertEquals(1, Person.get_count());
+
+        Person test2 = new Person("44444444");
+        assertNotEquals(1, Person.get_count());
+        assertTrue(2, Person.get_count());
     }
+
+
+    @Test
+    public void personClassesNeverAcceptInvalidStrings()
+    {
+        Person test_person = new Person("42082fybtp0rz");
+        assertFalse(test_person.validate_number(test_person.get_id()));
+    }
+
 
     @Test
     public void implementsLuhnFormulaCorrectly()
     {
-        Person ben = new Person("8888888888888888");
-        assertFalse(ben.validate_number(ben.get_id()));
+        Person test_person = new Person("8888888888888888");
+        assertFalse(test_person.validate_number(test_person.get_id()));
 
-        String card_number = "44444444";
-        assertTrue(ben.validate_number(card_number));
+        test_person.set_id("1111");
+        assertTrue(test_person.validate_number(test_person.get_id()));
+    }
+
+
+    @Test
+    public void evaluatesAnyPositiveLengthOfNumber()
+    {
+        Person test_person = new Person("4444444");
+        assertTrue(test_person.validate_number(test_person.get_id()));
+    }
+
+
+    @Test
+    public void doesNotAcceptEmptyStrings()
+    {
+        Person test_person = new Person("500");
+        assertFalse(test_person.set_id(""));
+        assertEquals(test_person.get_id(), "500");
     }
 }
+
