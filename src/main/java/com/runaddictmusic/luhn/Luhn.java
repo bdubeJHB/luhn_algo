0660e8e90b936d0be93205627ebe9a5bcf00e243
bdubeJHB <bdube@student.wethinkcode.co.za>
2021-01-20 19:01:19 +0200

    Refactored code to conform with Java best practices

--- src/main/java/com/runaddictmusic/luhn/Luhn.java
+++ src/main/java/com/runaddictmusic/luhn/Luhn.java
@@ -1,59 +1,61 @@
+/*
+ * Copyright (c) 2021 Benedict Dube. Creative Commons Attribution (CC BY)
+ * 
+ * This program should not be used as a final validation measure as it provides
+ * only basic validation of numbers and may let some invalid numbers through.
+ */
 package com.runaddictmusic.luhn;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
-public class Luhn
-{
-    public final static boolean luhn(String id_number)
-    {
+/**
+ * Luhn algorithm: Used to validate credit card numbers, IMEI numbers
+ * and certain countries ID numbers (including South Africa).
+ *
+ * @version 1.0 20 Jan 2021
+ * @author  Benedict Dube
+ */
+public class Luhn {
+    public final static boolean luhn(String idNumber) {
         Logger logger = LoggerFactory.getLogger(Luhn.class);
+        int sum = 0;
+        boolean checkDigit = false;
 
-        if(id_number == null || id_number.isEmpty())
-        {
+        if ((idNumber == null) || idNumber.isEmpty()) {
             logger.error("No valid input");
             return false;
         }
 
-        try
-        {
-            Integer.parseInt(id_number);
+        try {
+            Integer.parseInt(idNumber);
         }
-        catch(Exception e)
-        {
+        catch (Exception e) {
             logger.error("Number contains illegal charaters");
             return false;
         }
 
-        int sum = 0;
-        boolean check_digit = false;
-        for(int index = id_number.length() - 1; index >= 0; index--)
-        {
-            int current = Integer.valueOf(id_number.charAt(index));
-            if(check_digit)
-            {
-                if(current * 2 > 9)
-                {
+        for (int index = idNumber.length() - 1; index >= 0; index--) {
+            int current = Integer.valueOf(idNumber.charAt(index));
+
+            if (checkDigit) {
+                if (current * 2 > 9) {
                     current *= 2;
                     sum += current % 10;
                     sum += current / 10;
-                }
-                else
-                {
+                } else {
                     sum += current * 2;
                 }
-                check_digit = false;
-            }
-            else
-            {
+                checkDigit = false;
+            } else {
                 sum += current;
-                check_digit = true;
+                checkDigit = true;
             }
         }
 
-        logger.info("For input \"{}\", validation returned: {}", id_number, sum % 10 == 0);
-        return sum % 10 == 0 ? true: false;
+        logger.info("For input \"{}\", validation returned: {}",
+                    idNumber, sum % 10 == 0);
+        return (sum % 10 == 0) ? true: false;
     }
 }
 
